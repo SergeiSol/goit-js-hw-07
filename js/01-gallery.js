94c8863203a54318edba48d7047bc439a35643ef
SergeiSol <sergeisolovei4149@gmail.com>
2022-07-05 16:54:45 +0300

    2 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery")
 
-console.log(galleryItems);
+const galleryItemsRef = galleryItems
+    .map(({ preview, original, description }) => {
+        return `<div class = "gallery__item">
+    <a class = "gallery__link" href = "${original}">
+    <img class = "gallery__image" data-source ="${original}" src = "${preview}"
+    alt = "${description}"></a></div>`;
+    })
+    .join("");
+    
+    gallery.insertAdjacentHTML("beforeend", galleryItemsRef);
+
+    gallery.addEventListener("click", selectGalleryEl)
+const instance = basicLightbox.create(`<img src="" alt="">`, {
+    
+    onShow: () => document.addEventListener("keydown", onKeydownEsc),
+    onClose: () => document.removeEventListener("keydown", onKeydownEsc),
+   
+});
+
+function selectGalleryEl(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+
+        return;
+    }
+    const image = instance.element().querySelector("img");
+    image.src = event.target.dataset.source;
+    
+     instance.show()
+
+}
+   
+function onKeydownEsc(event) {
+    if (event.key === "Escape") {
+        instance.close();
+    }
+}
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
+console.log(galleryItems);
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryItemsRef = galleryItems
+    .map(({ preview, original, description }) => {
+        return `<div class = "gallery__item">
+    <a class = "gallery__link" href = "${original}">
+    <img class = "gallery__image" data-source ="${original}" src = "${preview}"
+    alt = "${description}"></a></div>`;
+    })
+    .join("");
+    
+    gallery.insertAdjacentHTML("beforeend", galleryItemsRef);
+
+
+const lightbox = new SimpleLightbox(".gallery a", { 
+    captionsPosition: "buttom",
+    captionsData: "alt",
+    captionsDelay: "250",
+     });
